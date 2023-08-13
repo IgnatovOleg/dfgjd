faf82a9321d43214a1c1b11a60955549cbaafffd
IgnatovOleg <08623.ignatovoleg@gmail.com>
2023-08-13 23:38:23 +0300

    fgfdgsdfsdf

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import HeaderHomePage from "../haderHomePage/HeaderHomePage";
 import ModalCreateProject from "../modalCreateProject/ModalCreateProject";
