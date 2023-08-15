d7e6f6de1a387be991cc0428e4caed1ee52e5739
IgnatovOleg <08623.ignatovoleg@gmail.com>
2023-08-15 23:27:49 +0300

    dsfsdgsdfassgsdg

--- src/store/reducers/usersReducer.ts
+++ src/store/reducers/usersReducer.ts
@@ -4,7 +4,7 @@ const defaultState: Iusers = {
     users: [
         {id: 1, login: "ignatovoleg", password: "ignatovOleg!", confirmPassword: "ignatovOleg!", firstName: "Oleg", lastName: "Ignatov", middleName: "Volodomorovich", email: "08623.ignatovoleg@gmail.com", phone: "380934352419"},
         {id: 2, login: "savchenkoserhiy", password: "savchenkoSerhiy!", confirmPassword: "savchenkoSerhiy!", firstName: "Serhiy", lastName: "Savchenko", middleName: "Anatolievich", email: "08623.ignatovoleg@gmail.com", phone: "38095432830"},
-        {id: 3, login: "ignatovaVika", password: "ignatovaVika!", confirmPassword: "ignatovaVika!", firstName: "Vika", lastName: "Ignatova", middleName: "Olexandrovna", email: "08623.ignatovoleg@gmail.com", phone: "380934352419"},
+        {id: 3, login: "ignatovaVika", password: "цй", confirmPassword: "ignatovaVika!", firstName: "Vika", lastName: "Ignatova", middleName: "Olexandrovna", email: "08623.ignatovoleg@gmail.com", phone: "380934352419"},
     ]
 }
 
